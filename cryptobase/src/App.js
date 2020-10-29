d93bb7deb2c791b78f153465ce9a61227122608b
istott <isaac.cloyd@gmail.com>
2020-10-28 18:39:13 -0600

    first commit, npm installed dependencies

---
+++ cryptobase/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+
+  return (
+    <div className="App"> 
+      <input
+        type='input'
+      />
+
+    </div>
+  );
+}
+
+export default App;

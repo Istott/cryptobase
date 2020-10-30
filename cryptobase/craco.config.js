2eece9a9eb5dcc3675d03842f1fbd3dc0d5c665e
istott <isaac.cloyd@gmail.com>
2020-10-29 23:51:59 -0600

    feat: add linaria styling library
    test linaria for functionality in app.js

---
+++ cryptobase/craco.config.js
@@ -0,0 +1,10 @@
+/* craco.config.js */
+const CracoLinariaPlugin = require('craco-linaria')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLinariaPlugin,
+    },
+  ],
+}
--- cryptobase/src/App.js
+++ cryptobase/src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+
+import { css } from 'linaria';
+import { styled } from 'linaria/react';
+
+const appStyle = css`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const MyButton = styled.button`
+  color: white;
+  /* border: 2px blue solid; */
+  border-radius: 5px;
+  background-color: lightseagreen;
+  margin: 1%;
+`;
 
 function App() {
 
   
 
   return (
-    <div className="App"> 
+    <div className={appStyle}>
+      <h1>Hello</h1> 
       <input
         type='input'
       />
 
+      <MyButton>Submit</MyButton>
     </div>
   );
 }

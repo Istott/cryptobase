e212d0a19940e0d1ba63e1151e31069b36fe35a0
istott <isaac.cloyd@gmail.com>
2020-11-05 15:21:52 -0700

    messing with typescript

--- cryptobase/src/App.js
+++ cryptobase/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 // import './App.css';
 
 import { css } from 'linaria';
@@ -29,8 +30,26 @@ function App() {
   const [name, setName] = useState({
     name: '',
   });
+  const [coinData, setCoinData] = useState([]);
 
+  
   console.log(name);
+  console.log(coinData);
+
+  const projectId = '527ad3693bdb489387c6e5f97dfbd16d'
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://mainnet.infura.io/v3/${projectId}`
+      )
+      .then(res => {
+        console.log(' API res', res)
+        // setCoinData(res.data)
+      })
+      .catch(err => console.log(err));
+  }, []);
+
 
   const handleChanges = event => {
     setName({ ...name, [event.target.name]: event.target.value});
